2e76e325325b87cfb38bad4a87293439b0272e8c
Colin <colintklein@gmail.com>
2020-07-21 13:23:36 -0500

    Adds model methods to access Yelp API

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,4 +4,15 @@ class Place < ApplicationRecord
   has_many :users, through: :favorites
   belongs_to :category
 
+  # Yelp methods
+  def details(yelp_id)
+    request = HTTP.get("https://api.yelp.com/v3/businesses/#{yelp_id}", :headers => { :Authorization => "Bearer #{Rails.application.credentials.yelp_api[:api_key]}"})
+    request.parse
+  end
+
+  def reviews(yelp_id)
+    request = HTTP.get("https://api.yelp.com/v3/businesses/#{yelp_id}/reviews", :headers => { :Authorization => "Bearer #{Rails.application.credentials.yelp_api[:api_key]}"})
+    request.parse
+  end
+
 end
