853a9a265af8de217ed6a7ead952fca2a9491417
Colin <colintklein@gmail.com>
2020-08-03 13:40:49 -0500

    Adds method to get seed data

--- app/models/place.rb
+++ app/models/place.rb
@@ -15,4 +15,19 @@ class Place < ApplicationRecord
     request.parse
   end
 
+  def self.yelp_seed
+    Place.all.each do |place|
+      if !place.api_place_id
+        response = HTTP.get("https://api.yelp.com/v3/businesses/search?location=Chicago&term=#{place.name.gsub(" ", "%20")}", :headers => { :Authorization => "Bearer #{Rails.application.credentials.yelp_api[:api_key]}"})
+        place.update(
+          api_place_id: response.parse["businesses"][0]["id"],
+          image_url: response.parse["businesses"][0]["image_url"],
+          lat: response.parse["businesses"][0]["coordinates"]["latitude"],
+          lon: response.parse["businesses"][0]["coordinates"]["longitude"])
+      end
+    end
+  end
+
 end
+
+# {"businesses"=>[{"id"=>"Imy7MhrTq0z-GQ5RjN_USg", "alias"=>"andysunflower-café-chicago-2", "name"=>"AndySunflower Café", "image_url"=>"https://s3-media2.fl.yelpcdn.com/bphoto/PmFDIyonCVWAcg8dsW6mtA/o.jpg", "is_closed"=>false, "url"=>"https://www.yelp.com/biz/andysunflower-caf%C3%A9-chicago-2?adjust_creative=-Vov8GvSxNfHKi8eLs9aew&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=-Vov8GvSxNfHKi8eLs9aew", "review_count"=>4, "categories"=>[{"alias"=>"coffee", "title"=>"Coffee & Tea"}], "rating"=>5.0, "coordinates"=>{"latitude"=>41.713268, "longitude"=>-87.682102}, "transactions"=>[], "location"=>{"address1"=>"9920 S Western Ave", "address2"=>"", "address3"=>nil, "city"=>"Chicago", "zip_code"=>"60643", "country"=>"US", "state"=>"IL", "display_address"=>["9920 S Western Ave", "Chicago, IL 60643"]}, "phone"=>"+13129613171", "display_phone"=>"(312) 961-3171", "distance"=>21356.051881427007}], "total"=>2, "region"=>{"center"=>{"longitude"=>-87.67776489257812, "latitude"=>41.90515925618311}}}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create(name: "Carribbean")
-Category.create(name: "American")
-Category.create(name: "Cafe")
+# Category.create(name: "Carribbean")
+# Category.create(name: "American")
+# Category.create(name: "Cafe")
 
-Place.create(name: "14 Parish Restaurant & Rhum Bar", api_place_id: "ic1X0dPQQK7VAJOsvxLYUQ", bar: true, image_url: "https://s3-media3.fl.yelpcdn.com/bphoto/2WYqzwalAfJ0tLlK_cqnYg/o.jpg", lat: 41.799815, lon: -87.584843, category_id: 1)
-Place.create(name: "5 Loaves Eatery", api_place_id: "V4UEL-OmlUHa6-hNpu4N6Q", bar: false, image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/AZBMmKqhw3-prt3v6QJmew/o.jpg", lat: 41.7582923, lon: -87.6147692, category_id: 2)
-Place.create(name: "Ain't She Sweet Cafe", api_place_id: "IRnNJNVlSyhQG0lBfzsZaA", bar: false, image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/99TZgCMxiLxeoLlDLJvL7w/o.jpg", lat: 41.8168655, lon: -87.612947, category_id: 3)
-Place.create(name: "AndySunflower Cafe", api_place_id: "Imy7MhrTq0z-GQ5RjN_USg", bar: false, image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/PmFDIyonCVWAcg8dsW6mtA/o.jpg", lat: 41.713268, lon: -87.682102, category_id: 3)
+# Place.create(name: "14 Parish Restaurant & Rhum Bar", api_place_id: "ic1X0dPQQK7VAJOsvxLYUQ", bar: true, image_url: "https://s3-media3.fl.yelpcdn.com/bphoto/2WYqzwalAfJ0tLlK_cqnYg/o.jpg", lat: 41.799815, lon: -87.584843, category_id: 1)
+# Place.create(name: "5 Loaves Eatery", api_place_id: "V4UEL-OmlUHa6-hNpu4N6Q", bar: false, image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/AZBMmKqhw3-prt3v6QJmew/o.jpg", lat: 41.7582923, lon: -87.6147692, category_id: 2)
+# Place.create(name: "Ain't She Sweet Cafe", api_place_id: "IRnNJNVlSyhQG0lBfzsZaA", bar: false, image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/99TZgCMxiLxeoLlDLJvL7w/o.jpg", lat: 41.8168655, lon: -87.612947, category_id: 3)
+# Place.create(name: "AndySunflower Cafe", api_place_id: "Imy7MhrTq0z-GQ5RjN_USg", bar: false, image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/PmFDIyonCVWAcg8dsW6mtA/o.jpg", lat: 41.713268, lon: -87.682102, category_id: 3)
+
+Place.create(name: "B&B's Ice Cream and Candy", category_id: 3)
+Place.create(name: "BigCity CheeseSteaks", category_id: 2)
 
 
-User.create(name: "testname", email: "testemail", password_digest: "testdigest")
+# User.create(name: "testname", email: "testemail", password_digest: "testdigest")
 
-Favorite.create(user_id: 1, place_id: 1)
-Favorite.create(user_id: 1, place_id: 2)
-Favorite.create(user_id: 1, place_id: 4)
+# Favorite.create(user_id: 1, place_id: 1)
+# Favorite.create(user_id: 1, place_id: 2)
+# Favorite.create(user_id: 1, place_id: 4)
